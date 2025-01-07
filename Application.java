ed6ae488cdb91094f746dfad0409aa1f2987756b
Tsunamy1996 <umberto.giacobbe96@libero.it>
2025-01-07 11:35:30 +0100

    creazione frame

--- Application.java
+++ Application.java
@@ -1,5 +1,11 @@
+import components.Frame.LoginFrame;
+
 public class Application {
         public static void main(String[] args) {
-            // Your code here
+         
+         
+         LoginFrame frame = new LoginFrame();
+         
+            
         }
     }
